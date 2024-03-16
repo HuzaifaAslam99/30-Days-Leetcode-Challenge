7db376a97fdb41d622c31804fe46a349e38f7001
HuzaifaAslam99 <162318658+HuzaifaAslam99@users.noreply.github.com>
2024-03-16 12:51:04 +0500

    Time: 2 ms (13.29%), Space: 40 MB (89.78%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-               if (num < 0) {
+        if (num < 0) {
             return false;
         }
 
-        // Iterate up to the square root of the number
-        for (long i = 0; i * i <= num; i++) {
-            // Check if the square of the current iteration is equal to the number
+        for (long i = 0; i * i <= num; i++){
             if (i * i == num) {
                 return true;
             }
